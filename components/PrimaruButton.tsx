c054de04669a3ac05e8c921d9ca798460e247ee1
Anastasiia Kulachok <anastasiia@MacBookAir.fritz.box>
2025-07-03 17:20:03 +0200

    added PrimaruButton component

---
+++ components/PrimaruButton.tsx
@@ -0,0 +1,48 @@
+import React from 'react';
+import { View, Text, Pressable, StyleSheet } from 'react-native';
+
+import { Colors } from '../constants/colors';
+
+interface PrimaryButtonProps {
+  children: React.ReactNode;
+  onPress: () => void;
+}
+
+const PrimaryButton: React.FC<PrimaryButtonProps> = ({ children, onPress }) => {
+  return (
+      <Pressable
+        style={({ pressed }) =>
+          pressed
+            ? [styles.buttonInnerContainer, styles.pressed]
+            : styles.buttonInnerContainer
+        }
+        onPress={onPress}
+        android_ripple={{ color: Colors.primaryRed}}
+      >
+        <Text style={styles.buttonText}>{children}</Text>
+      </Pressable>
+  );
+};
+
+export default PrimaryButton;
+
+const styles = StyleSheet.create({
+  buttonInnerContainer: {
+    backgroundColor: Colors.primaryRed,
+    paddingVertical: 12,
+    paddingHorizontal: 16,
+    elevation: 2,
+
+     borderRadius: 28,
+     width: '80%',
+
+  },
+  buttonText: {
+    color: 'white',
+    textAlign: 'center',
+    fontSize: 16,
+  },
+  pressed: {
+    opacity: 0.75,
+  },
+});
