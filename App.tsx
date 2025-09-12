35cf644b1c3dbcf605b3c110d07533f33bcdacc0
anastasiia <kulachokn@gmail.com>
2025-09-12 09:43:43 +0200

    dependencies fixed

--- App.tsx
+++ App.tsx
@@ -304,7 +304,7 @@ function MainTabs() {
         options={{
           tabBarLabel: "Eintragen",
           tabBarIcon: ({ color }) => (
-            <AntDesign name="pluscircleo" size={24} color={color} />
+            <AntDesign name="plus-circle" size={24} color={color} />
           ),
         }}
       />
--- components/InfoRow.tsx
+++ components/InfoRow.tsx
@@ -1,13 +1,13 @@
 import { View, Text, StyleSheet, Pressable } from "react-native";
 import he from "he";
-import { AntDesign, Feather, Ionicons } from "@expo/vector-icons";
+import { AntDesign, Feather, Ionicons, FontAwesome5 } from "@expo/vector-icons";
 
 import { formatShortDate, formatTimeHHMM } from "../utils/formatFunctions";
 import { Colors } from "../constants/colors";
 import { EventOnEvent } from "../types/event";
 
-type InfoRowProps = { 
-  event: EventOnEvent; 
+type InfoRowProps = {
+  event: EventOnEvent;
   openInMaps?: () => void;
 };
 
@@ -21,31 +21,29 @@ export function InfoRow({ event, openInMaps }: InfoRowProps) {
   return (
     <View style={styles.infoRow}>
       <View style={styles.infoItem}>
-        <AntDesign name="calendar" size={24} color={Colors.text800} />
+        <FontAwesome5 name="calendar-alt" size={24} color={Colors.text800} />
         <Text style={styles.infoText}>{date}</Text>
       </View>
       <View style={styles.infoItem}>
         <Feather name="clock" size={24} color={Colors.text800} />
         <Text style={styles.infoText}>{time}</Text>
       </View>
-      <Pressable 
+      <Pressable
         style={[
-          styles.infoItem, 
-          { opacity: locationName === "Ort unbekannt" ? 0.7 : 1 }
+          styles.infoItem,
+          { opacity: locationName === "Ort unbekannt" ? 0.7 : 1 },
         ]}
         onPress={openInMaps}
         disabled={!openInMaps || locationName === "Ort unbekannt"}
         android_ripple={openInMaps ? { color: "rgba(0,0,0,0.1)" } : undefined}
       >
-        <Ionicons 
-          name="location-outline" 
-          size={25} 
-          color={Colors.text800}
-        />
-        <Text 
+        <Ionicons name="location-outline" size={25} color={Colors.text800} />
+        <Text
           style={[
-            styles.infoText, 
-            openInMaps && locationName !== "Ort unbekannt" ? { textDecorationLine: 'underline' } : {}
+            styles.infoText,
+            openInMaps && locationName !== "Ort unbekannt"
+              ? { textDecorationLine: "underline" }
+              : {},
           ]}
           numberOfLines={2}
         >
